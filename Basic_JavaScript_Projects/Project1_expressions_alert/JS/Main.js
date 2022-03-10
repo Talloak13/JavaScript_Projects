04e1b46b981bb30fe5d4c9bf4dc2b979eaf19eb5
Talloak13 <94999648+Talloak13@users.noreply.github.com>
2022-03-10 10:31:36 -0800

    Update to Java projects

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/Main.js
@@ -0,0 +1,15 @@
+var X = "\"Hello, World!\"" + "I hope you\'re having a nice day", Y = "Woot!"
+
+window.alert (X);
+
+document.write (Y);
+
+document.write (3+3); // This is an expression inside a statement
+
+function My_Frist_Function() {
+    var str = "This is the button text!";
+    document.getElementById("Button_Text").innerHTML = str;
+}
+let text = "Holy Cow!";
+let result = text.fontcolor("green"); //Not completely sure why this still works but I tried other ways and I couldn't get the text green using JavaScript
+document.getElementById("demo1").innerHTML = result;
